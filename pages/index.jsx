0de5d0f0860f19274efd33597e21626a7ad785a3
boon4681 <boon4681dev@gmail.com>
2025-05-19 01:28:49 +0700

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,23 +3,29 @@ import styles from '../styles/Home.module.css'
 import { useEffect } from 'react'
 
 export default function Home() {
-    useEffect(()=>{
+    useEffect(() => {
         window.location.href = 'https://www.youtube.com/watch?v=xvFZjo5PgG0'
     })
+    const title = "boon4681 (@boon4681) • Instagram profile"
+    const url = "https://ig.boon4681.com/"
+    const description = "540 Followers, 545 Following, 9 Posts - See Instagram photos and videos from boon4681"
+    const image = "/kokowa.png"
     return (
         <div className={styles.container}>
             <Head>
-                <title>Login • Instagram</title>
+                <title>{title}</title>
                 <meta name="theme-color" content="#FFFFFF" />
-                <meta name="title" content="Login • Instagram" />
-                <meta name="description" content="Welcome back to Instagram. Sign in to check out what your friends, family & interests have been capturing & sharing around the world." />
+                <meta name="title" content={title} />
+                <meta name="description" content={description} />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://ig.boon4681.com/" />
-                <meta property="og:title" content="Login • Instagram" />
-                <meta property="og:description" content="Welcome back to Instagram. Sign in to check out what your friends, family & interests have been capturing & sharing around the world." />
-                <meta property="twitter:url" content="https://ig.boon4681.com/" />
-                <meta property="twitter:title" content="Login • Instagram" />
-                <meta property="twitter:description" content="Welcome back to Instagram. Sign in to check out what your friends, family & interests have been capturing & sharing around the world." />
+                <meta property="og:url" content={url} />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta property="og:image" content={image}></meta>
+                <meta property="twitter:url" content={url} />
+                <meta property="twitter:title" content={title} />
+                <meta property="twitter:description" content={description} />
+                <meta property="twitter:image" content={image}></meta>
             </Head>
         </div>
     )
